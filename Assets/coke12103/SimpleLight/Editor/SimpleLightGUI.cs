fdaea10b2e8b5bf51043bbdcea81ab1b8b751068
coke12103 <coke12103@gmail.com>
2021-08-25 21:00:28 +0900

    update animation create func prototype

--- Assets/coke12103/SimpleLight/Editor/SimpleLightGUI.cs
+++ Assets/coke12103/SimpleLight/Editor/SimpleLightGUI.cs
@@ -608,7 +608,6 @@ public class SimpleLightGUI : EditorWindow
         string color_name = ColorUtility.ToHtmlStringRGB(template_colors[i]);
 
         if(light_mode == 2){
-          Debug.Log(template_color_anims[i]);
           AddCurve(template_color_anims[i], target_light_spot.gameObject.transform, typeof(Light), "m_Color.r", r);
           AddCurve(template_color_anims[i], target_light_spot.gameObject.transform, typeof(Light), "m_Color.g", g);
           AddCurve(template_color_anims[i], target_light_spot.gameObject.transform, typeof(Light), "m_Color.b", b);
@@ -635,16 +634,55 @@ public class SimpleLightGUI : EditorWindow
       if(target_light_point != null) target_light_point.color = single_color;
     }
 
+    if(strength_mode == 0){
+      // とりあえずサイズ2で生成
+      Transform[] lights = new Transform[2];
+
+      if(light_mode == 2){
+        lights[0] = target_light_spot.gameObject.transform;
+        lights[1] = target_light_point.gameObject.transform;
+      }else{
+        lights[0] = (light_mode == 0 ? target_light_spot : target_light_point).gameObject.transform;
+        System.Array.Resize(ref lights, 1);
+      }
+
+      BlendTree strength_tree = CreateBlendTree("Strength", prefix + "Strength", lights, typeof(Light), "m_Intensity", min_strength, max_strength);
+
+      CreateState(fx_layer, prefix + "Strength", strength_tree);
+    }else if(strength_mode == 1){
+      AnimationClip[] template_strength_anims = new AnimationClip[template_strengths.Length];
+
+      for(int i = 0; i < template_strength_anims.Length; i++){
+        template_strength_anims[i] = new AnimationClip();
+
+        float val = template_strengths[i];
+
+        if(light_mode == 2){
+          AddCurve(template_strength_anims[i], target_light_spot.gameObject.transform, typeof(Light), "m_Intensity", val);
+          AddCurve(template_strength_anims[i], target_light_point.gameObject.transform, typeof(Light), "m_Intensity", val);
+        }else{
+          Transform target = (light_mode == 0 ? target_light_spot : target_light_point).gameObject.transform;
+
+          AddCurve(template_strength_anims[i], target, typeof(Light), "m_Intensity", val);
+        }
+
+        AssetDatabase.CreateAsset(template_strength_anims[i], user_asset_path + "/strength_" + val.ToString() + ".anim");
+
+        AnimatorState state = CreateState(fx_layer, prefix + "Strength", template_strength_anims[i]);
+
+        AnimatorStateTransition transition = CreateAnyStateTransition(fx_layer, prefix + "Strength", state);
+
+        transition.AddCondition(AnimatorConditionMode.Equals, i, prefix + "Strength");
+      }
+    }else{
+      if(target_light_spot != null) target_light_spot.intensity = single_strength;
+      if(target_light_point != null) target_light_point.intensity = single_strength;
+    }
+
     // NOTE: 何故かFXをSetDirtyしなくてもちゃんと反映される
     AssetDatabase.SaveAssets();
     AssetDatabase.Refresh();
 
-
-//
-//    // float strength / int strength
-//    if(strength_mode == 0 || strength_mode == 1){
-//      fx_layer.AddParameter(prefix + "Strength", strength_mode == 0 ? AnimatorControllerParameterType.Float : AnimatorControllerParameterType.Int);
-//    }
 //
 //    // float range / int range
 //    if(range_mode == 0 || range_mode == 1){
@@ -697,6 +735,34 @@ public class SimpleLightGUI : EditorWindow
     anim.layers = layers;
 
     return transition;
+  }
+
+  BlendTree CreateBlendTree(string name, string param, Transform[] targets, System.Type target_type, string value_key, float min, float max){
+    string low_name = name.ToLower();
+
+    BlendTree tree = new BlendTree();
+
+    AnimationClip zero_anim = new AnimationClip();
+    AnimationClip one_anim = new AnimationClip();
+
+    tree.name = name;
+    tree.blendType = BlendTreeType.Simple1D;
+    tree.blendParameter = param;
+
+    for(int i = 0; i < targets.Length; i++){
+      Transform target = targets[i];
+
+      AddCurve(zero_anim, target, target_type, value_key, min);
+      AddCurve(one_anim, target, target_type, value_key, max);
+    }
+
+    AssetDatabase.CreateAsset(zero_anim, user_asset_path + "/" + low_name + "_zero.anim");
+    AssetDatabase.CreateAsset(one_anim, user_asset_path + "/" + low_name + "_one.anim");
+
+    tree.AddChild(zero_anim, 0);
+    tree.AddChild(one_anim, 1);
+
+    return tree;
   }
 
   BlendTree CreateBlendTree(string name, string param){
